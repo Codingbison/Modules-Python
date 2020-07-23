dc25b612de8403cd3a3dcb6d5b77c4e356823d83
Nibras M <nawaraaa2000@yahoo.com>
2020-07-23 06:36:39 -0700

    second attempt

---
+++ solution2/Circle.py
@@ -0,0 +1,10 @@
+
+def circle():
+    if rad == 'x':
+        exit();
+    else:
+        radius - float(rad);
+        circumference = 2*3.14*radius;
+        print(\"nCircumference of Circle =", circumference);
+
+circle()
---
+++ solution2/sqaurefootage.py
@@ -0,0 +1,5 @@
+def square_footage(l, w):
+    area = l * w
+    return area
+
+square_footage()
